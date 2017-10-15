66182aef73c2d31b20ef422a00638535e36d86b0
Évelyne Lachance <eslachance@gmail.com>
2017-10-15 18:33:39 -0400

    Episode 3 code

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const Discord = require("discord.js");
+/* eslint consistent-return: 0, no-console: 0 */
+const Discord = require('discord.js');
+
+const config = require('./config.json');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,9 +10,24 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-  if (message.content === 'ping') {
-    message.reply('Pong!');
+  if (message.author.bot) return;
+
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    message.channel.send('Pong...').then((msg) => {
+      msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    });
+  }
+
+  if (command === 'speak') {
+    if (message.author.id !== config.ownerID) return message.reply('Arrooo???');
+    message.channel.send(args.join(' '));
+    message.delete();
   }
 });
 
-client.login('this token has since been deleted');
+client.login(config.token);
